c5b09ef0caa1723c75fc2cb56382c17ca6ad0550
lakshya112-stack <laks8281@gmail.com>
2026-06-24 23:58:55 +0530

    Updated project

--- 5.ShifraAI/Client/src/utils/firebase.js
+++ 5.ShifraAI/Client/src/utils/firebase.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "shifraai.firebaseapp.com",
-  projectId: "shifraai",
-  storageBucket: "shifraai.firebasestorage.app",
-  messagingSenderId: "1095099448118",
-  appId: "1:1095099448118:web:6cbcb1b989228c5fc99197"
+  authDomain: "ai-voice-agent-d0628.firebaseapp.com",
+  projectId: "ai-voice-agent-d0628",
+  storageBucket: "ai-voice-agent-d0628.firebasestorage.app",
+  messagingSenderId: "788974380879",
+  appId: "1:788974380879:web:f6cc9abe13fc187e30873a",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app)
-const provider = new GoogleAuthProvider()
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export {auth , provider}
-
+export { auth, provider };
