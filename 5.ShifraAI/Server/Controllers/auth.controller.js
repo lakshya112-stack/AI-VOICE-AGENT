e9dc8e54dbcec890fe98c60fc3c380cbb04eb40b
lakshya112-stack <laks8281@gmail.com>
2026-06-25 08:52:28 +0530

    Show auth error

--- 5.ShifraAI/Server/Controllers/auth.controller.js
+++ 5.ShifraAI/Server/Controllers/auth.controller.js
@@ -26,7 +26,10 @@ export const googleAuth = async (req,res) => {
         return res.status(200).json(user)
 
     } catch (error) {
-        return res.status(500).json({message:`Google auth error ${error}`})
+        console.error(error);
+        return res.status(500).json({
+            message: error.message
+        });
     }
 }
 
